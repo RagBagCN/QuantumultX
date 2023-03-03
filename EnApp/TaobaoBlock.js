3f4621d27e772aae68ce5b14a2fe38f0353435b5
RagBag <73032662+RagBagCN@users.noreply.github.com>
2023-03-03 17:00:25 +0800

    Update TaobaoBlock.js

--- EnApp/TaobaoBlock.js
+++ EnApp/TaobaoBlock.js
@@ -1,20 +1,20 @@
-(function () {
-  function blockUpdate() {
-    const originalFetch = fetch;
-    fetch = function (input, init) {
-      if (typeof input === 'string' && input.includes('api.m.taobao.com') && input.includes('mtop.upgrade.Upgrade')) {
-        console.log('[TaobaoBlock.js] Blocked update check request:', input);
-        return new Promise(() => {});
-      } else {
-        return originalFetch.apply(this, arguments);
-      }
-    };
-    console.log('[TaobaoBlock.js] Update check blocking enabled.');
-  }
+const updateCheckUrl = 'https://loggw-ex.alipay.com/antfarm/updates.json?appName=tb&appVersion=9.10.0.20001485';
 
-  if (window.fetch) {
-    blockUpdate();
-  } else {
-    document.addEventListener('DOMContentLoaded', blockUpdate);
+$task.fetch({
+  url: updateCheckUrl,
+  headers: {
+    'User-Agent': 'TB9.10.0.20001485 CFNetwork/1220.1 Darwin/20.3.0',
+    'Accept-Language': 'zh-cn'
   }
-})();
+}).then(response => {
+  const data = JSON.parse(response.body);
+  if (data.isNewVersion === 'NO') {
+    $notify('淘宝', '已经是最新版本', '');
+  }
+  else {
+    console.log(`发现新版本：${data.updateVersion}`);
+    $notify('淘宝', `发现新版本：${data.updateVersion}`, '');
+  }
+}, reason => {
+  console.log(reason.error);
+});
