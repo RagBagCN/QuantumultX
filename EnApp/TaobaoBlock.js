ee51e1631a22df886add6ccb0f85eecae0594957
RagBag <73032662+RagBagCN@users.noreply.github.com>
2023-03-03 17:28:18 +0800

    Update TaobaoBlock.js

--- EnApp/TaobaoBlock.js
+++ EnApp/TaobaoBlock.js
@@ -1,11 +1,1 @@
-var body = $response.body;
-body = JSON.parse(body);
-if (body && body.data && body.data.match && body.data.match.upgrade && body.data.match.upgrade.prompt_type) {
-    body.data.match.upgrade.prompt_type = 2;
-    body.data.match.upgrade.is_show_btn = false;
-    body.data.match.upgrade.upgrade_button_info = {};
-    body.data.match.upgrade.upgrade_button_info.upgrade_button_text = "";
-    body.data.match.upgrade.upgrade_button_info.upgrade_button_schema = "";
-}
-body = JSON.stringify(body);
-$done({body});
+$done('{}');
