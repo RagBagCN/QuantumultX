675b0d7691a894588b2407e8399db53c157b859e
RagBag <73032662+RagBagCN@users.noreply.github.com>
2023-03-03 17:12:15 +0800

    Update TaobaoBlock.js

--- EnApp/TaobaoBlock.js
+++ EnApp/TaobaoBlock.js
@@ -1,20 +1,11 @@
-const updateCheckUrl = 'https://loggw-ex.alipay.com/antfarm/updates.json?appName=tb&appVersion=9.10.0.20001485';
-
-$task.fetch({
-  url: updateCheckUrl,
-  headers: {
-    'User-Agent': 'TB9.10.0.20001485 CFNetwork/1220.1 Darwin/20.3.0',
-    'Accept-Language': 'zh-cn'
-  }
-}).then(response => {
-  const data = JSON.parse(response.body);
-  if (data.isNewVersion === 'NO') {
-    $notify('淘宝', '已经是最新版本', '');
-  }
-  else {
-    console.log(`发现新版本：${data.updateVersion}`);
-    $notify('淘宝', `发现新版本：${data.updateVersion}`, '');
-  }
-}, reason => {
-  console.log(reason.error);
-});
+var body = $response.body;
+body = JSON.parse(body);
+if (body && body.data && body.data.match && body.data.match.upgrade && body.data.match.upgrade.prompt_type) {
+    body.data.match.upgrade.prompt_type = 2;
+    body.data.match.upgrade.is_show_btn = false;
+    body.data.match.upgrade.upgrade_button_info = {};
+    body.data.match.upgrade.upgrade_button_info.upgrade_button_text = "";
+    body.data.match.upgrade.upgrade_button_info.upgrade_button_schema = "";
+}
+body = JSON.stringify(body);
+$done({body});
