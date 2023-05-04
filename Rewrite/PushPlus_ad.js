907eec70de4be83eba935fc425d1055f805eed05
RagBag <73032662+RagBagCN@users.noreply.github.com>
2023-05-04 13:37:20 +0800

    Update PushPlus_ad.js

--- Rewrite/PushPlus_ad.js
+++ Rewrite/PushPlus_ad.js
@@ -1,16 +1,17 @@
-function removeAd(html) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  const adLink = doc.querySelector('a[href="https://u.qushaia.cn/2a04"]');
+const url = $request.url;
+if (!$response.body) $done({});
 
-  if (adLink) {
-    const container = adLink.closest('.container');
-    if (container) {
-      container.remove();
-    }
+const parser = new DOMParser();
+const doc = parser.parseFromString($response.body, 'text/html');
+const adLink = doc.querySelector('a[href="https://u.qushaia.cn/2a04"]');
+
+if (adLink) {
+  const container = adLink.closest('.container');
+  if (container) {
+    container.remove();
   }
-
-  return new XMLSerializer().serializeToString(doc);
 }
 
-$done({body: removeAd($response.body)});
+const newBody = new XMLSerializer().serializeToString(doc);
+$done({ body: newBody });
+
